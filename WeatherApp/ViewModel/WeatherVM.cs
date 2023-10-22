b6e71f73b930c8b974218748aaed62110f2d9efe
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 18:06:09 +0200

    Add weather view model defining inside the on property changed trigger

---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            //Trigger the event of specified property changed
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
