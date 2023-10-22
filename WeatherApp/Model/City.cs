e96f0d42f28edf30bfd1a221f40e096218957512
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 16:46:51 +0200

    Update City model adding inside this file the classes: - Area - City

--- WeatherApp/Model/City.cs
+++ WeatherApp/Model/City.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace WeatherApp.Model
 {
-    internal class City
+    public class Area
     {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
     }
 }
