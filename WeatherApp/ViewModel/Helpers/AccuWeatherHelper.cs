0f342b594b50fa2e41510174c8e67a263f38754c
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 17:08:59 +0200

    Add accu wheater view model helper with the base url and autocomplete end point constants

---
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "/locations/v1/cities/autocomplete?apikey={0}&q={1}&language=it-IT";
+    }
+}
