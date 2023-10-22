87bc1dae5b46bacca61ac0e79154ca3aec7db46e
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 18:08:45 +0200

    Update weather view model, define the query property and call inside the setter the on property changed event handler

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -9,6 +9,18 @@ namespace WeatherApp.ViewModel
 {
     public class WeatherVM : INotifyPropertyChanged
     {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set { 
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         private void OnPropertyChanged(string propertyName)
