75a1f1cf4ee31500a690cae03948f9a22675ba50
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 17:13:36 +0200

    Add condition edpoint constant in the accu wheather helper

--- WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -9,7 +9,8 @@ namespace WeatherApp.ViewModel.Helpers
     public class AccuWeatherHelper
     {
         public const string BASE_URL = "http://dataservice.accuweather.com/";
-        public const string AUTOCOMPLETE_ENDPOINT = "/locations/v1/cities/autocomplete?apikey={0}&q={1}&language=it-IT";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}&language=it-IT";
+        public const string CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}&language=it-IT";
         public const string API_KEY = "OVTXLxbNzHMcEJ8G8QpDqUaWYusMFiIq";
     }
 }
