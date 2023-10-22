6292c55fc284f8ca91def82a02c9e46a348e98e5
totarochristian <totaro.christian.18@gmail.com>
2023-10-22 18:53:06 +0200

    Update search command: - add weather view model property - add constructor with instance of weather view model - return always true on CanExecute method - call the make query method in the Execute method of this search command

--- WeatherApp/ViewModel/Commands/SearchCommand.cs
+++ WeatherApp/ViewModel/Commands/SearchCommand.cs
@@ -9,16 +9,25 @@ namespace WeatherApp.ViewModel.Commands
 {
     public class SearchCommand:ICommand
     {
+        public WeatherVM VM { get; set; }
+
         public event EventHandler? CanExecuteChanged;
+
+        public SearchCommand(WeatherVM vm)
+        {
+            //Assign the weather view model instance as default
+            VM = vm;
+        }
 
         public bool CanExecute(object? parameter)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            //Execute the query to auto complete the selected city
+            VM.MakeQuery();
         }
     }
 }
